4d0014e46eeb375d64c537dbf995f98399d16a9f
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2023-01-30 18:11:57 +0100

    fix: gitignore

---
+++ .history/src/utils/fetch-data-api/fetch-data-api.service_20230130153007.ts
@@ -0,0 +1,22 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class FetchDataApiService {
+  private url = 'http://api.weatherstack.com/current';
+  private weatherApi = 'd203f24abc57cfb788c3f51154affdc2';
+
+  constructor(private readonly httpService: HttpService) {}
+
+  public async getDataFromApi(query: any, type: any) {
+    const params = {
+      access_key: this.weatherApi,
+      query,
+    };
+    const { data } = await firstValueFrom(
+      this.httpService.get(this.url, { params }),
+    );
+    return data;
+  }
+}
---
+++ .history/src/utils/fetch-data-api/fetch-data-api.service_20230130165016.ts
@@ -0,0 +1,42 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class FetchDataApiService {
+  private weatherUrl = 'https://api.open-meteo.com/v1/forecast';
+  private geoUrl = 'https://geocoding-api.open-meteo.com/v1/search';
+  private weatherApi = 'd203f24abc57cfb788c3f51154affdc2';
+
+  constructor(private readonly httpService: HttpService) {}
+
+  public async getDataFromApi(countryName: string) {
+    const { latitude, longitude } = await this.getGeoLocation(countryName);
+    const params = {
+      latitude,
+      longitude,
+      current_weather: true,
+    };
+    const { data } = await firstValueFrom(
+      this.httpService.get(this.weatherUrl, { params }),
+    );
+    return data;
+  }
+
+  public async getGeoLocation(name: string) {
+    const params = {
+      name,
+      count: 1,
+    };
+
+    const {
+      data: {
+        results: [{ latitude, longitude }],
+      },
+    } = await firstValueFrom(this.httpService.get(this.geoUrl, { params }));
+    return {
+      latitude,
+      longitude,
+    };
+  }
+}
